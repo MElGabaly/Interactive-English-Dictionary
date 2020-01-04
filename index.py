18a67928d9b6b9cc3c77541ccbbdf022541aa752
Mo <Gabalyma@gmail.com>
2020-01-03 20:38:45 -0800

    Program Working

--- index.py
+++ index.py
@@ -10,13 +10,28 @@ def search(user_input):
     user_input = user_input.lower()
     if user_input in data:
         return data[user_input]
-    # to find a close match
+    # To find a close match
     elif len(get_close_matches(user_input, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(user_input, data.keys())[0]
+        close_word = get_close_matches(user_input, data.keys())[0]
+        response = input("Did you mean '%s' instead? (Y/N) " % close_word)
+        if response == "Y":
+            return data[close_word]
+        elif response == "N":
+            return "Sorry we couldn't help you!! "
+        else:
+            return "Please Enter (Y) if Yes, or (N) if No"
     else:
         return "The Word Doesn't exist, Please Double check it"
 
 
 # User Interfacing
 user_input = input("Enter Word: ")
-print(search(user_input))
+
+# output
+outputs = search(user_input)
+
+if type(outputs) == list:
+    for output in outputs:
+        print(output)
+else:
+    print(output)
